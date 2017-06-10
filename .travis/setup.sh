de54b3fcedc0d1798bfa0d9f05d9d1522acbb123
Paul Mason <paul@form1.co.nz>
2017-06-09 21:12:10 -0700

    Downgrading postgres to 9.4

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 sudo cp pg_hba.conf $(psql -U postgres -c "SHOW hba_file" -At)
 
 sudo service postgresql stop
-sudo service postgresql start 9.6
+sudo service postgresql start 9.4
