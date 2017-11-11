84838af5c194e4ff102200e51a761bcb78b0b032
Paul Mason <paul@form1.co.nz>
2017-11-11 09:32:57 -0800

    Working dec! macro implementation, nightly only

--- decimal_macro/tests/macro_tests.rs
+++ decimal_macro/tests/macro_tests.rs
@@ -5,6 +5,9 @@ use rust_decimal_macro::*;
 
 #[test]
 fn it_can_parse_decimal() {
-    let d = d!(123.45);
-    assert_eq!("123.45", d.to_string());
+    assert_eq!("0.00", dec!(0.00).to_string());
+    assert_eq!("1.00", dec!(1.00).to_string());
+    assert_eq!("-1.23", dec!(-1.23).to_string());
+    assert_eq!("1.1234567890123456789012345678", 
+        dec!(1.1234567890123456789012345678).to_string());
 }
