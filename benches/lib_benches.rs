2a7eb7fc40db90e30153da8e315841d3a5882fe4
Paul Mason <paul@form1.co.nz>
2017-12-29 16:38:35 -0800

    Added in automatic benchmarking for PR's and pushes

--- benches/lib_benches.rs
+++ benches/lib_benches.rs
@@ -2,16 +2,16 @@
 
 extern crate test;
 extern crate rust_decimal;
+#[cfg(feature = "comparitive")]
 extern crate decimal;
-use decimal::d128;
+
 use rust_decimal::Decimal;
 use std::str::FromStr;
-use test::Bencher;
 
 macro_rules! bench_bin_op {
     ($name:ident, $ty:ident, $op:tt) => {
         #[bench]
-        fn $name(b: &mut Bencher) {
+        fn $name(b: &mut ::test::Bencher) {
             b.iter(|| {
 
                 let y = $ty::from_str("2").unwrap();
@@ -29,13 +29,17 @@ macro_rules! bench_bin_op {
 }
 
 bench_bin_op!(bench_decimal_add, Decimal, +=);
-bench_bin_op!(bench_d128_add, d128, +=);
-
 bench_bin_op!(bench_decimal_sub, Decimal, -=);
-bench_bin_op!(bench_d128_sub, d128, -=);
-
 bench_bin_op!(bench_decimal_mul, Decimal, *=);
-bench_bin_op!(bench_d128_mul, d128, *=);
-
 bench_bin_op!(bench_decimal_div, Decimal, /=);
-bench_bin_op!(bench_d128_div, d128, /=);
+
+#[cfg(feature = "comparitive")]
+mod comparitive {
+    use decimal::d128;
+    use super::*;
+
+    bench_bin_op!(bench_d128_add, d128, +=);
+    bench_bin_op!(bench_d128_sub, d128, -=);
+    bench_bin_op!(bench_d128_mul, d128, *=);
+    bench_bin_op!(bench_d128_div, d128, /=);
+}
