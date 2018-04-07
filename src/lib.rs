85ca3a48759f044691934af6affc95a8345fd6e0
Paul Mason <paul@form1.co.nz>
2018-04-06 20:39:49 -0700

    Fix cargo doc warnings

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,12 @@
 //! Decimal numbers can be created in a few distinct ways, depending
 //! on the rust compiler version you're targeting.
 //!
-//! ### Stable
-//!
 //! The stable version of rust requires you to create a Decimal number
 //! using one of it's convenience methods.
 //!
 //! ```rust
 //! use rust_decimal::Decimal;
+//! use std::str::FromStr;
 //!
 //! // Using an integer followed by the decimal points
 //! let scaled = Decimal::new(202, 2); // 2.02
@@ -34,20 +33,6 @@
 //! // Using the raw decimal representation
 //! // 3.1415926535897932384626433832
 //! let pi = Decimal::from_parts(1102470952, 185874565, 1703060790, false, 28);
-//! ```
-//!
-//! ### Nightly
-//!
-//! With the nightly version of rust you can use a procedural macro using
-//! the rust_decimal_macros crate. The advantage of this method is that the
-//! decimal numbers are parsed at compile time.
-//!
-//! ```rust
-//! // Procedural macros need importing directly
-//! use rust_decimal_macros::*;
-//!
-//! let number = dec!(-1.23);
-//!
 //! ```
 //!
 extern crate num;
