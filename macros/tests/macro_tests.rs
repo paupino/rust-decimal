e85f89b655f95cb063b5f74e0e413f2da283c9af
Paul Mason <paul@form1.co.nz>
2018-05-26 09:55:34 -0700

    Fixes nightly tests by declaring feature for proc macro expr

--- macros/tests/macro_tests.rs
+++ macros/tests/macro_tests.rs
@@ -1,3 +1,4 @@
+#![feature(proc_macro_non_items)]
 extern crate rust_decimal;
 extern crate rust_decimal_macros;
 
@@ -8,6 +9,6 @@ fn it_can_parse_decimal() {
     assert_eq!("0.00", dec!(0.00).to_string());
     assert_eq!("1.00", dec!(1.00).to_string());
     assert_eq!("-1.23", dec!(-1.23).to_string());
-    assert_eq!("1.1234567890123456789012345678", 
+    assert_eq!("1.1234567890123456789012345678",
         dec!(1.1234567890123456789012345678).to_string());
 }
