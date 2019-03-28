4a52f483843139c2b67449f8fa50cb6ca261636d
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-03-28 15:30:37 -0400

    Added default to decimal

--- src/decimal.rs
+++ src/decimal.rs
@@ -1031,6 +1031,12 @@ impl Decimal {
     }
 }
 
+impl Default for Decimal {
+    fn default() -> Self {
+        Self::zero()
+    }
+}
+
 #[inline]
 #[cfg(feature = "const_fn")]
 const fn flags(neg: bool, scale: u32) -> u32 {
