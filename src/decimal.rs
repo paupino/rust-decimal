319221f6216ac7c4660547c00cb6d26b720945b5
Paul Mason <paul@form1.co.nz>
2020-09-13 17:24:17 -0700

    Added rounding before returning from to_f64

--- src/decimal.rs
+++ src/decimal.rs
@@ -2662,7 +2662,9 @@ impl ToPrimitive for Decimal {
             let integral_part = mantissa / precision;
             let frac_part = mantissa % precision;
             let frac_f64 = (frac_part as f64) / (precision as f64);
-            Some(sign * ((integral_part as f64) + frac_f64))
+            let value = sign * ((integral_part as f64) + frac_f64);
+            let round_to = (10u128.pow(self.scale())) as f64;
+            Some(value * round_to / round_to)
         }
     }
 }
