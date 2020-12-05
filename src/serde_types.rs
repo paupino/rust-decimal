b2eaf1ee1630e367eafab4e73b3b09debd3c3ca3
Paul Mason <paul@paulmason.me>
2020-12-04 17:53:28 -0800

    Add arbitrary precision support for serde_json deserialization

--- src/serde_types.rs
+++ src/serde_types.rs
@@ -34,6 +34,10 @@ impl<'de> serde::Deserialize<'de> for Decimal {
     }
 }
 
+// It's a shame this needs to be redefined for this feature and not able to be referenced directly
+#[cfg(feature = "serde-arbitrary-precision")]
+const DECIMAL_KEY_TOKEN: &str = "$serde_json::private::Number";
+
 struct DecimalVisitor;
 
 impl<'de> serde::de::Visitor<'de> for DecimalVisitor {
@@ -77,6 +81,90 @@ impl<'de> serde::de::Visitor<'de> for DecimalVisitor {
         Decimal::from_str(value)
             .or_else(|_| Decimal::from_scientific(value))
             .map_err(|_| E::invalid_value(Unexpected::Str(value), &self))
+    }
+
+    #[cfg(feature = "serde-arbitrary-precision")]
+    fn visit_map<A>(self, map: A) -> Result<Decimal, A::Error>
+    where
+        A: serde::de::MapAccess<'de>,
+    {
+        let mut map = map;
+        let value = map.next_key::<DecimalKey>()?;
+        if value.is_none() {
+            return Err(serde::de::Error::invalid_type(Unexpected::Map, &self));
+        }
+        let v: DecimalFromString = map.next_value()?;
+        Ok(v.value)
+    }
+}
+
+#[cfg(feature = "serde-arbitrary-precision")]
+struct DecimalKey;
+
+#[cfg(feature = "serde-arbitrary-precision")]
+impl<'de> serde::de::Deserialize<'de> for DecimalKey {
+    fn deserialize<D>(deserializer: D) -> Result<DecimalKey, D::Error>
+    where
+        D: serde::de::Deserializer<'de>,
+    {
+        struct FieldVisitor;
+
+        impl<'de> serde::de::Visitor<'de> for FieldVisitor {
+            type Value = ();
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a valid decimal field")
+            }
+
+            fn visit_str<E>(self, s: &str) -> Result<(), E>
+            where
+                E: serde::de::Error,
+            {
+                if s == DECIMAL_KEY_TOKEN {
+                    Ok(())
+                } else {
+                    Err(serde::de::Error::custom("expected field with custom name"))
+                }
+            }
+        }
+
+        deserializer.deserialize_identifier(FieldVisitor)?;
+        Ok(DecimalKey)
+    }
+}
+
+#[cfg(feature = "serde-arbitrary-precision")]
+pub struct DecimalFromString {
+    pub value: Decimal,
+}
+
+#[cfg(feature = "serde-arbitrary-precision")]
+impl<'de> serde::de::Deserialize<'de> for DecimalFromString {
+    fn deserialize<D>(deserializer: D) -> Result<DecimalFromString, D::Error>
+    where
+        D: serde::de::Deserializer<'de>,
+    {
+        struct Visitor;
+
+        impl<'de> serde::de::Visitor<'de> for Visitor {
+            type Value = DecimalFromString;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("string containing a decimal")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<DecimalFromString, E>
+            where
+                E: serde::de::Error,
+            {
+                let d = Decimal::from_str(value)
+                    .or_else(|_| Decimal::from_scientific(value))
+                    .map_err(serde::de::Error::custom)?;
+                Ok(DecimalFromString { value: d })
+            }
+        }
+
+        deserializer.deserialize_str(Visitor)
     }
 }
 
@@ -125,7 +213,7 @@ mod test {
             assert_eq!(
                 true,
                 result.is_ok(),
-                "expected successful deseralization for {}. Error: {:?}",
+                "expected successful deserialization for {}. Error: {:?}",
                 serialized,
                 result.err().unwrap()
             );
@@ -138,6 +226,14 @@ mod test {
                 record.amount.to_string()
             );
         }
+    }
+
+    #[test]
+    #[cfg(feature = "serde-arbitrary-precision")]
+    fn deserialize_basic_decimal() {
+        let d: Decimal = serde_json::from_str("1.1234127836128763").unwrap();
+        // Typically, this would not work without this feature enabled due to rounding
+        assert_eq!(d.to_string(), "1.1234127836128763");
     }
 
     #[test]
