9365805c5f2c3cc98d44b486b6ba309e52114a70
Caio <c410.f3r@gmail.com>
2021-06-07 21:08:19 -0300

    Update arithmetic.rs
    My bad. The `?` operator immediately returns the closure so, for example, an input that could spot a bug on the last arithmetic function will never be exercised if the first function returns early.

--- fuzz/fuzz_targets/arithmetic.rs
+++ fuzz/fuzz_targets/arithmetic.rs
@@ -1,6 +1,6 @@
 #![no_main]
 
-use rust_decimal::{Decimal, MathematicalOps};
+use rust_decimal::Decimal;
 
 #[derive(Debug, arbitrary::Arbitrary)]
 struct Data {
@@ -12,20 +12,15 @@ struct Data {
 }
 
 libfuzzer_sys::fuzz_target!(|data: Data| {
-    let fun = || {
-        let _ = data.a.checked_add(data.b)?;
-        let _ = data.a.checked_div(data.b)?;
-        //let _ = data.a.checked_exp_with_tolerance(data.b)?;
-        //let _ = data.a.checked_exp()?;
-        //let _ = data.a.checked_mul(data.b)?;
-        //let _ = data.a.checked_norm_pdf()?;
-        //let _ = data.a.checked_powd(data.b)?;
-        //let _ = data.a.checked_powf(data.exp_f64)?;
-        //let _ = data.a.checked_powi(data.exp_i64)?;
-        //let _ = data.a.checked_powu(data.exp_u64)?;
-        //let _ = data.a.checked_sub(data.b)?;
-
-        Some(())
-    };
-    let _ = fun();
+    let _ = data.a.checked_add(data.b);
+    let _ = data.a.checked_div(data.b);
+    //let _ = data.a.checked_exp_with_tolerance(data.b);
+    //let _ = data.a.checked_exp();
+    //let _ = data.a.checked_mul(data.b);
+    //let _ = data.a.checked_norm_pdf();
+    //let _ = data.a.checked_powd(data.b);
+    //let _ = data.a.checked_powf(data.exp_f64);
+    //let _ = data.a.checked_powi(data.exp_i64);
+    //let _ = data.a.checked_powu(data.exp_u64);
+    //let _ = data.a.checked_sub(data.b);
 });
