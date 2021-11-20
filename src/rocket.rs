92b7fcb5b4fbfc0140183745b4a5f1826815d4c5
Gabriel Fontes <eu@misterio.me>
2021-11-20 15:01:53 -0300

    Don't provide failback value when parsing

--- src/rocket.rs
+++ src/rocket.rs
@@ -1,10 +1,10 @@
 use crate::Decimal;
-use rocket::form::{self, FromFormField, ValueField};
+use rocket::{form::{self, FromFormField, ValueField}, request::FromParam};
 use std::str::FromStr;
 
 impl<'v> FromFormField<'v> for Decimal {
     fn default() -> Option<Self> {
-        Default::default()
+        None
     }
     fn from_value(field: ValueField<'v>) -> form::Result<'v, Self> {
         Decimal::from_str(field.value).map_err(|_| form::Error::validation("not a valid number").into())
