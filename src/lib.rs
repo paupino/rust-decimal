3e771d908d1d4875e78a09505c47bda5fb14fa6c
James Collins <jracollins@gmail.com>
2021-12-20 16:53:31 +0000

    added num_traits `Signed` to prelude (#450)

--- src/lib.rs
+++ src/lib.rs
@@ -203,7 +203,7 @@ pub mod prelude {
     pub use crate::maths::MathematicalOps;
     pub use crate::{Decimal, RoundingStrategy};
     pub use core::str::FromStr;
-    pub use num_traits::{FromPrimitive, One, ToPrimitive, Zero};
+    pub use num_traits::{FromPrimitive, One, Signed, ToPrimitive, Zero};
 }
 
 #[cfg(feature = "diesel")]
