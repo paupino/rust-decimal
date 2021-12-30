05a925146bb1beb712914ddc87ae323592f61971
Tomatillo <tomatillo@cantortrading.fi>
2021-12-30 13:56:56 +0800

    Use 64 bit integer with 128 bit fallback to speed up further
    Accumulate parse state into a 64-bit integer until we would observe an
    overflow of this integer, and fall back to maintaining 128-bit state
    
    cargo bench --bench lib_benches -- decimal_from_str
    
    Baseline:
    decimal_from_str    ... bench:     566 ns/iter (+/- 21) [M1 Pro]
    decimal_from_str    ... bench:     554 ns/iter (+/- 21) [Ryzen 3990x]
    
    Current:
    decimal_from_str    ... bench:     133 ns/iter (+/- 4) [M1 Pro]
    decimal_from_str    ... bench:     190 ns/iter (+/- 5) [Ryzen 3990x]

--- src/str.rs
+++ src/str.rs
@@ -132,37 +132,101 @@ pub(crate) fn parse_str_radix_10(str: &str) -> Result<Decimal, crate::Error> {
         return Err(Error::from("Invalid decimal: empty"));
     }
 
-    let bytes = str.as_bytes();
+    let mut bytes = str.as_bytes();
     let mut negative = false; // assume positive
 
     // handle the sign
-    let bytes = if bytes[0] == b'-' {
-        negative = true; // leading minus means negative
-        &bytes[1..]
-    } else if bytes[0] == b'+' {
-        // leading + allowed
-        &bytes[1..]
-    } else {
-        bytes
+
+    bytes = match bytes[0] {
+        b'-' => {
+            negative = true; // leading minus means negative
+            &bytes[1..]
+        }
+        b'+' => {
+            // leading + allowed
+            &bytes[1..]
+        }
+        _ => bytes,
     };
 
     // should now be at numeric part of the significand
-    let len = bytes.len();
-    let mut offset = 0;
     let mut passed_decimal_point = false;
     let mut has_digit = false;
-    let mut data: u128 = 0;
-    let mut coeff: u32 = 0; // number of digits
-    let mut scale = 0;
-    let mut maybe_round = false;
+    let mut coeff: u8 = 0; // number of digits
+    let mut scale: u8 = 0;
 
-    while offset < len {
-        let b = bytes[offset];
+    let mut data64: u64 = 0;
+
+    while data64 < (u64::MAX / 10 - u8::MAX as u64) {
+        // no overflow can happen here!
+        if !bytes.is_empty() {
+            let b = bytes[0];
+            match b {
+                b'0'..=b'9' => {
+                    let digit = u32::from(b - b'0');
+                    has_digit = true;
+                    coeff += if coeff == 0 && digit == 0 { 0 } else { 1 }; // got one more digit
+
+                    // we have already validated that we cannot overflow
+                    data64 = data64 * 10 + digit as u64;
+                    scale += passed_decimal_point as u8;
+                }
+                b'.' => {
+                    if passed_decimal_point {
+                        return Err(Error::from("Invalid decimal: two decimal points"));
+                    }
+                    passed_decimal_point = true;
+                }
+                b'_' => {
+                    // Must start with a number...
+                    if !has_digit {
+                        return Err(Error::from("Invalid decimal: must start lead with a number"));
+                    }
+                }
+                _ => return Err(Error::from("Invalid decimal: unknown character")),
+            }
+            bytes = &bytes[1..];
+
+            debug_assert!(coeff < MAX_PRECISION as u8);
+            if scale >= 28 {
+                if !bytes.is_empty() {
+                    return maybe_round(data64 as u128, bytes[0], scale, negative, passed_decimal_point);
+                }
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    let data: u128 = data64 as u128;
+
+    if !bytes.is_empty() {
+        return handle_full_128(data, bytes, scale, coeff, passed_decimal_point, negative);
+    } else if !has_digit {
+        return Err(Error::from("Invalid decimal: no digits found"));
+    }
+
+    handle_data(data, scale, negative)
+}
+
+#[inline(never)]
+#[cold]
+fn handle_full_128(
+    mut data: u128,
+    mut bytes: &[u8],
+    mut scale: u8,
+    mut coeff: u8,
+    mut passed_decimal_point: bool,
+    negative: bool,
+) -> Result<Decimal, crate::Error> {
+    while !bytes.is_empty() {
+        debug_assert!(coeff > 0);
+        let b = bytes[0];
         match b {
             b'0'..=b'9' => {
                 let digit = u32::from(b - b'0');
-                has_digit = true;
-                coeff += if coeff == 0 && digit == 0 { 0 } else { 1 }; // got one more digit
+                coeff += 1; // got one more digit
 
                 // If the data is going to overflow then we should go into recovery mode
                 let next = data * 10;
@@ -184,12 +248,10 @@ pub(crate) fn parse_str_radix_10(str: &str) -> Result<Decimal, crate::Error> {
                     break;
                 } else {
                     data = next + digit as u128;
-                    if passed_decimal_point {
-                        scale += 1;
-                    }
+                    scale += passed_decimal_point as u8;
                     if overflows(data) {
                         // Highly unlikely scenario which is more indicative of a bug
-                        return Err(Error::from("Invalid decimal: overflow from carry"));
+                        return Err(Error::from("Invalid decimal: overflow when rounding"));
                     }
                 }
             }
@@ -199,56 +261,62 @@ pub(crate) fn parse_str_radix_10(str: &str) -> Result<Decimal, crate::Error> {
                 }
                 passed_decimal_point = true;
             }
-            b'_' => {
-                // Must start with a number...
-                if offset == 0 {
-                    return Err(Error::from("Invalid decimal: must start lead with a number"));
-                }
-            }
+            b'_' => {}
             _ => return Err(Error::from("Invalid decimal: unknown character")),
         }
-        offset += 1;
+        bytes = &bytes[1..];
 
-        if coeff > MAX_PRECISION as u32 || scale >= 28 {
-            maybe_round = true;
+        if coeff > MAX_PRECISION as u8 || scale >= 28 {
+            if !bytes.is_empty() {
+                return maybe_round(data, bytes[0], scale, negative, passed_decimal_point);
+            }
             break;
         }
     }
 
-    if !has_digit {
-        return Err(Error::from("Invalid decimal: no digits found"));
-    }
+    handle_data(data, scale, negative)
+}
 
-    // If we exited before the end of the string then do some rounding if necessary
-    if maybe_round && offset < bytes.len() {
-        let next_byte = bytes[offset];
-        let digit = match next_byte {
-            b'0'..=b'9' => u32::from(next_byte - b'0'),
-            b'_' => 0, // this is a bug?
-            b'.' => {
-                // Still an error if we have a second dp
-                if passed_decimal_point {
-                    return Err(Error::from("Invalid decimal: two decimal points"));
-                }
-                0
+#[inline(never)]
+#[cold]
+fn maybe_round(
+    mut data: u128,
+    next_byte: u8,
+    scale: u8,
+    negative: bool,
+    passed_decimal_point: bool,
+) -> Result<Decimal, crate::Error> {
+    let digit = match next_byte {
+        b'0'..=b'9' => u32::from(next_byte - b'0'),
+        b'_' => 0, // this is a bug?
+        b'.' => {
+            // Still an error if we have a second dp
+            if passed_decimal_point {
+                return Err(Error::from("Invalid decimal: two decimal points"));
             }
-            _ => return Err(Error::from("Invalid decimal: unknown character")),
-        };
+            0
+        }
+        _ => return Err(Error::from("Invalid decimal: unknown character")),
+    };
 
-        // Round at midpoint
-        if digit >= 5 {
-            data += 1;
-            if overflows(data) {
-                // Highly unlikely scenario which is more indicative of a bug
-                return Err(Error::from("Invalid decimal: overflow when rounding"));
-            }
+    // Round at midpoint
+    if digit >= 5 {
+        data += 1;
+        if overflows(data) {
+            // Highly unlikely scenario which is more indicative of a bug
+            return Err(Error::from("Invalid decimal: overflow when rounding"));
         }
     }
 
+    handle_data(data, scale, negative)
+}
+
+fn handle_data(data: u128, scale: u8, negative: bool) -> Result<Decimal, crate::Error> {
     debug_assert_eq!(data >> 96, 0);
+
     let data = [data as u32, (data >> 32) as u32, (data >> 64) as u32];
 
-    Ok(Decimal::from_parts(data[0], data[1], data[2], negative, scale))
+    Ok(Decimal::from_parts(data[0], data[1], data[2], negative, scale as u32))
 }
 
 pub(crate) fn parse_str_radix_n(str: &str, radix: u32) -> Result<Decimal, crate::Error> {
