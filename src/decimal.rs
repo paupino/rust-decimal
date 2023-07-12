524711a362b63fde3114c0889e262853c51addf0
Mikhail Katychev <mkatych@gmail.com>
2023-07-12 17:37:38 -0500

    added new rkyv

--- src/decimal.rs
+++ src/decimal.rs
@@ -120,7 +120,7 @@ pub struct UnpackedDecimal {
     archive(compare(PartialEq)),
     archive_attr(derive(Clone, Copy, Debug))
 )]
-#[cfg_attr(feature = "rkyv-safe", archive_attr(derive(bytecheck::CheckBytes)))]
+#[cfg_attr(feature = "rkyv-safe", archive(check_bytes))]
 pub struct Decimal {
     // Bits 0-15: unused
     // Bits 16-23: Contains "e", a value between 0-28 that indicates the scale
