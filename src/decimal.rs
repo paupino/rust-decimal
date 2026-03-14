56810d4046dc167b5631caee9d04b94187731bc4
Paul Mason <paul@paulmason.me>
2026-03-13 20:52:47 -0700

    Add bytemuck feature to README, fix duplicate derive

--- src/decimal.rs
+++ src/decimal.rs
@@ -116,7 +116,6 @@ pub struct UnpackedDecimal {
     archive_attr(derive(Clone, Copy, Debug))
 )]
 #[cfg_attr(feature = "rkyv-safe", archive(check_bytes))]
-#[cfg_attr(feature = "bytemuck", derive(bytemuck_derive::Pod, bytemuck_derive::Zeroable))]
 #[cfg_attr(all(target_arch = "wasm32", feature = "wasm"), wasm_bindgen)]
 pub struct Decimal {
     // Bits 0-15: unused
