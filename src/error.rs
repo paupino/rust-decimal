4f59c3d8bfce6d49e71a7f40eb227cf006e7521e
Caio <c410.f3r@gmail.com>
2026-03-14 00:47:21 -0300

    Allow the usage of fallible operations in constant environments (#750)

--- src/error.rs
+++ src/error.rs
@@ -1,43 +1,39 @@
 use crate::Decimal;
-use alloc::string::String;
 use core::fmt;
 
 /// Error type for the library.
 #[derive(Clone, Debug, PartialEq)]
 pub enum Error {
-    /// A generic error from Rust Decimal with the `String` containing more information as to what
-    /// went wrong.
-    ///
-    /// This is a legacy/deprecated error type retained for backwards compatibility.  
-    ErrorString(String),
+    /// Represents a failure to convert to/from `Decimal` to the specified type. This is typically
+    /// due to type constraints (e.g. `Decimal::MAX` cannot be converted into `i32`).
+    ConversionTo(&'static str),
+    /// The decimal string contained more than one decimal point.
+    DuplicatedDecimalPoint,
+    /// Could not represent a Decimal instance because there no data left.
+    EmptyData,
     /// The value provided exceeds `Decimal::MAX`.
     ExceedsMaximumPossibleValue,
     /// A string could not represent a scientific number.
     FailedToParseScientificFromString,
+    /// A character could not represent a Decimal instance
+    InvalidCharacter,
+    /// The string must start with a digit, `+` or `-`.
+    InvalidLeadingChar,
     /// The value provided is less than `Decimal::MIN`.
     LessThanMinimumPossibleValue,
-    /// An underflow is when there are more fractional digits than can be represented within `Decimal`.
-    Underflow,
+    /// The string did not contain any digits.
+    NoDigits,
     /// The scale provided exceeds the maximum scale that `Decimal` can represent.
     ScaleExceedsMaximumPrecision(u32),
-    /// Represents a failure to convert to/from `Decimal` to the specified type. This is typically
-    /// due to type constraints (e.g. `Decimal::MAX` cannot be converted into `i32`).
-    ConversionTo(&'static str),
-}
-
-impl<S> From<S> for Error
-where
-    S: Into<String>,
-{
-    #[inline]
-    fn from(from: S) -> Self {
-        Self::ErrorString(from.into())
-    }
+    /// An underflow is when there are more fractional digits than can be represented within `Decimal`.
+    Underflow,
+    /// The radix is not supported. Must be between 2 and 36.
+    UnsupportedRadix,
 }
 
 #[cold]
-pub(crate) fn tail_error(from: &'static str) -> Result<Decimal, Error> {
-    Err(from.into())
+pub(crate) fn tail_error(from: Error) -> Result<Decimal, Error> {
+    Err(from)
 }
 
 impl core::error::Error for Error {}
@@ -45,18 +41,32 @@ impl core::error::Error for Error {}
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
-            Self::ErrorString(ref err) => f.pad(err),
+            Self::ConversionTo(ref type_name) => {
+                write!(f, "Error while converting to {type_name}")
+            }
+            Self::DuplicatedDecimalPoint => {
+                write!(f, "The decimal string contained more than one decimal point.")
+            }
+            Self::EmptyData => {
+                write!(f, "Could not represent a Decimal instance because there no data left.")
+            }
             Self::ExceedsMaximumPossibleValue => {
                 write!(f, "Number exceeds maximum value that can be represented.")
             }
             Self::FailedToParseScientificFromString => {
                 write!(f, "A string could not represent a scientific number.")
             }
+            Self::InvalidCharacter => {
+                write!(f, "A character could not represent a Decimal instance.")
+            }
+            Self::InvalidLeadingChar => {
+                write!(f, "The string must start with a digit, `+` or `-`.")
+            }
             Self::LessThanMinimumPossibleValue => {
                 write!(f, "Number less than minimum value that can be represented.")
             }
-            Self::Underflow => {
-                write!(f, "Number has a high precision that can not be represented.")
+            Self::NoDigits => {
+                write!(f, "The string did not contain any digits.")
             }
             Self::ScaleExceedsMaximumPrecision(ref scale) => {
                 write!(
@@ -65,8 +75,11 @@ impl fmt::Display for Error {
                     Decimal::MAX_SCALE
                 )
             }
-            Self::ConversionTo(ref type_name) => {
-                write!(f, "Error while converting to {type_name}")
+            Self::Underflow => {
+                write!(f, "Number has a high precision that can not be represented.")
+            }
+            Self::UnsupportedRadix => {
+                write!(f, "The radix is not supported. Must be between 2 and 36.")
             }
         }
     }
